ec7e589887b3e83a692048f31cbf1df0bc87ffeb
Татьяна Симченко <misstaty8181@gmail.com>
2022-10-21 16:17:13 +0200

    add checkout login register

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,126 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet.js";
+import CommonSection from "../components/UI/common-section/CommonSection";
+
+import "../styles/checkout.css";
 
 const Checkout = () => {
-  return (
-    <div>
-      Checkout
-    </div>
-  )
-}
+  const [enterName, setEnterName] = useState("");
+  const [enterEmail, setEnterEmail] = useState("");
+  const [enterNumber, setEnterNumber] = useState("");
+  const [enterCountry, setEnterCountry] = useState("");
+  const [enterCity, setEnterCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
 
-export default Checkout
+
+  const shippingInfo = [];
+  const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const shippingCoast = 30;
+  const totalAmount = cartTotalAmount + Number(shippingCoast);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const userShippingAdress = {
+      name: enterName,
+      email: enterEmail,
+      phone: enterNumber,
+      country: enterCountry,
+      city: enterCity,
+      postalCode: postalCode,
+    };
+    shippingInfo.push(userShippingAdress);
+    console.log(shippingInfo);
+  };
+  return (
+    <Helmet title="checkout">
+      <CommonSection title="Checkout" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8" md="6">
+              <h6 className="mb-4">Shipping Adress</h6>
+              <form className="checkout__form" onSubmit={submitHandler}>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Enter your name"
+                    required
+                    onChange={(e) => setEnterName(e.target.value)}
+                  />
+                </div>
+
+                <div className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    required
+                    onChange={(e) => setEnterEmail(e.target.value)}
+                  />
+                </div>
+                
+
+                <div className="form__group">
+                  <input
+                    type="number"
+                    placeholder="Phone number"
+                    required
+                    onChange={(e) => setEnterNumber(e.target.value)}
+                  />
+                </div>
+
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Country"
+                    required
+                    onChange={(e) => setEnterCountry(e.target.value)}
+                  />
+                </div>
+
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="City"
+                    required
+                    onChange={(e) => setEnterCity(e.target.value)}
+                  />
+                </div>
+
+                <div className="form__group">
+                  <input
+                    type="number"
+                    placeholder="Postal code"
+                    required
+                    onChange={(e) => setPostalCode(e.target.value)}
+                  />
+                </div>
+                <button className="addToCart__btn">Payment</button>
+              </form>
+             
+            </Col>
+
+            <Col lg="4" md="6">
+              <div className="checkout__bill">
+                <h6 className="d-flex align-items-center justify-content-between mb-3">
+                  Subtotal: <span>${cartTotalAmount}</span>
+                </h6>
+                <h6 className="d-flex align-items-center justify-content-between mb-3">
+                  Shipping: <span>${shippingCoast}</span>
+                </h6>
+                <div className="checkout__total">
+                  <h5 className="d-flex align-items-center justify-content-between">
+                    Total: <span>${totalAmount}</span>
+                  </h5>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Checkout;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,41 @@
+import React, { useRef } from 'react';
+import Helmet from "../components/Helmet/Helmet.js";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-
-import React from 'react'
 
 const Login = () => {
+
+  const loginNameref = useRef()
+  const loginPasswordRef = useRef()
+
+  const submitHandler = (e) => {
+e.preventDefault()
+  }
   return (
-    <div>
-      Login
-    </div>
+   <Helmet title="login">
+    <CommonSection title="Login"/>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6' md='6' sm='12' className='m-auto text-center'>
+          <form className="form mb-5" onSubmit={submitHandler}>
+            <div className='form__group'>
+              <input type="email" placeholder='Email' required ref={loginNameref}/>
+            </div>
+            <div className='form__group'>
+              <input type="password" placeholder='Password' required ref={loginPasswordRef}/>
+            </div>
+            <button type='submit' className='addToCart__btn'>Login</button>
+          </form>
+          <Link to='/register'>Don't have an account? Create an account</Link>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+   </Helmet>
   )
 }
 
