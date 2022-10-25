8b2d5492c700870a2db178afc0dbe8a09716a839
Татьяна Симченко <misstaty8181@gmail.com>
2022-10-25 09:31:14 +0200

    add some images

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -22,6 +22,7 @@ const AllFoods = () => {
       return false
     }
   },{});
+  
 
   const productPerPage = 8;
   const visitedPage = pageNumber * productPerPage;
@@ -54,15 +55,15 @@ const AllFoods = () => {
               </div>
             </Col>
             <Col lg="6" md="6" sm="6" className="mb-5">
-              <div className="sorting__widget text-end">
+              {/* <div className="sorting__widget text-end">
                 <select className="w-50">
                   <option>Default</option>
                   <option value="ascending">Alphabetically, A-Z</option>
                   <option value="descending">Alphabetically, Z-A</option>
-                  <option value="high-price">High Price</option>
+                  <option value="high-price" >High Price</option>
                   <option value="low-price">Low Price</option>
                 </select>
-              </div>
+              </div> */}
             </Col>
             {displayPage.map((item) => (
               <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
